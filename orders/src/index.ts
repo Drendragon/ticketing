88f784f9952270274577db7c4fc50c13ea1d2a85
Drendragon <drendragon.project@gmail.com>
2022-11-06 19:55:01 +0100

    Update index.ts

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,7 +9,7 @@ import {PaymentCreatedListener} from "./events/listeners/payment-created-listene
 
 const bootstrap = async () => {
 
-    console.log("Starting...");
+    console.log("Starting....");
 
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY is missing");
